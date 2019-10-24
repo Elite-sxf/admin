dc922596c618c58d44bc2cc81b4d9d28819546af
elite <1399826164@qq.com>
2019-10-24 16:42:28 +0800

    index基本

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,131 @@
 <template>
-  <div>
-    index
+  <div class="index">
+    <el-container>
+      <el-header>
+        <div class="logo">
+          <img src="../assets/logo.png" alt />
+        </div>
+        <div class="title">电商后台管理系统</div>
+        <div class="logout">
+          欢迎光临
+          <a href="javascript:;" @click="logout">退出</a>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            router
+            unique-opened
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>用户管理</span>
+              </template>
+              <el-menu-item index="users">
+                <i class="el-icon-menu"></i>
+                <span slot="title">用户列表</span>
+              </el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>权限管理</span>
+              </template>
+              <el-menu-item index="roles">
+                <i class="el-icon-menu"></i>
+                <span slot="title">角色列表</span>
+              </el-menu-item>
+              <el-menu-item index="rights">
+                <i class="el-icon-menu"></i>
+                <span slot="title">权限列表</span>
+              </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout () {
+      this.$confirm('确定要退出吗, 亲?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        localStorage.removeItem('token')
+        this.$router.push('/login')
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+.index {
+  width: 100%;
+  height: 100%;
+}
+.el-container {
+  width: 100%;
+  height: 100%;
+  .el-header {
+    height: 60px;
+    background-color: #d8d8d8;
+    display: flex;
+    .logo {
+      width: 180px;
+      padding: 10px;
+      img {
+        height: 40px;
+      }
+    }
+    .title {
+      text-align: center;
+      line-height: 60px;
+      font-weight: 700;
+      font-size: 24px;
+      text-align: center;
+      flex: 1;
+      color: hotpink;
+    }
+    .logout {
+      width: 180px;
+      text-align: right;
+      line-height: 60px;
+      font-weight: 700;
+      a {
+        color: #daa520;
+      }
+    }
+  }
 
+  .el-aside {
+    height: 100%;
+    background-color: #545c64;
+
+    .el-menu {
+      border: none;
+    }
+  }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,23 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/login.vue'
 import Index from '../components/index.vue'
+import Users from '../components/users.vue'
+import Roles from '../components/roles.vue'
+import Rights from '../components/rights.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/index' },
   { path: '/login', component: Login },
-  { path: '/index', component: Index }
+  { path: '/index',
+    component: Index,
+    children: [
+      { path: '/users', component: Users },
+      { path: '/roles', component: Roles },
+      { path: '/rights', component: Rights }
+    ]
+  }
 ]
 
 const router = new VueRouter({
